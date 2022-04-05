59f6f5e5f3bc6610179b64975493f6070587a466
Antoine WATTIAUX <awattiaux@auchan.fr>
2022-04-05 11:50:23 +0200

    add token gestion

--- src/store/module/authentication/getters.ts
+++ src/store/module/authentication/getters.ts
@@ -1,7 +1,7 @@
 import { IState } from './types'
 
   const isAuthenticated = (state: IState): boolean => !!state.token
-  
+
   const authStatus = (state: IState): string => state.authStatus
 
   const logoutStatus = (state: IState): string => state.logoutStatus
